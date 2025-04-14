6df17f0a697e325a02c37a68966c41de11b8fb96
Jeff Huang <jeffishjeff@gmail.com>
2025-04-14 17:57:36 +0800

    debug scripts

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,3 +1,5 @@
+source ../.env
+
 temp_txt="temp.txt"
 touch $temp_txt
 
@@ -10,14 +12,14 @@ for chain in $DEPLOY_CHAINS; do
     --etherscan-api-key "${!etherscan_var}" \
     --broadcast \
     --verify \
-    --sig 'run(string,address,string)' "${chain}" "${DEPLOY_INITIAL_OWNER}" "${temp_txt}"
+    --sig 'run(string,address,string)' "${chain}" "${DEPLOY_INITIAL_OWNER}" "script/${temp_txt}"
 
     forge script DeployUniswapV4AcrossSettler.s.sol:DeployUniswapV4AcrossSettler \
     --rpc-url "${!rpc_var}" \
     --etherscan-api-key "${!etherscan_var}" \
     --broadcast \
     --verify \
-    --sig 'run(string,address,string)' "${chain}" "${DEPLOY_INITIAL_OWNER}" "${temp_txt}"
+    --sig 'run(string,address,string)' "${chain}" "${DEPLOY_INITIAL_OWNER}" "script/${temp_txt}"
 done
 
 for chain in $DEPLOY_CHAINS; do
@@ -29,14 +31,14 @@ for chain in $DEPLOY_CHAINS; do
     --etherscan-api-key "${!etherscan_var}" \
     --broadcast \
     --verify \
-    --sig 'run(string,address,string)' "${chain}" "${DEPLOY_INITIAL_OWNER}" "${temp_txt}"
+    --sig 'run(string,address,string)' "${chain}" "${DEPLOY_INITIAL_OWNER}" "script/${temp_txt}"
 
     forge script script/DeployUniswapV4AcrossMigrator.s.sol:DeployUniswapV4AcrossMigrator \
     --rpc-url "${!rpc_var}" \
     --etherscan-api-key "${!etherscan_var}" \
     --broadcast \
     --verify \
-    --sig 'run(string,address,string)' "${chain}" "${DEPLOY_INITIAL_OWNER}" "${temp_txt}"
+    --sig 'run(string,address,string)' "${chain}" "${DEPLOY_INITIAL_OWNER}" "script/${temp_txt}"
 done
 
 rm $temp_txt
