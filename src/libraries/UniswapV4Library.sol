a1f0b2d56c589d8e74ca40f80a4f3e6ab146bb2d
Jeff Huang <jeffishjeff@gmail.com>
2025-05-03 18:13:24 +0800

    SB01 - early return for zero address

--- src/libraries/UniswapV4Library.sol
+++ src/libraries/UniswapV4Library.sol
@@ -227,6 +227,7 @@ library UniswapV4Library {
         address spender,
         uint256 amount
     ) internal {
+        if (currency.isAddressZero()) return;
         if (!isPermit2Approved[currency]) {
             IERC20(Currency.unwrap(currency)).forceApprove(address(permit2), type(uint256).max);
             isPermit2Approved[currency] = true;
