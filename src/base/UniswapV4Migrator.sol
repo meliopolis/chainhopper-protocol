2e24928c2666b68f807da6f8ecd68309ccd4ab17
Jeff Huang <jeffishjeff@gmail.com>
2025-05-03 16:11:22 +0800

    SB20 - immutable Uniswap addresses

--- src/base/UniswapV4Migrator.sol
+++ src/base/UniswapV4Migrator.sol
@@ -2,23 +2,31 @@
 pragma solidity ^0.8.24;
 
 import {IERC721Receiver} from "@openzeppelin/token/ERC721/IERC721Receiver.sol";
+import {IPermit2} from "@uniswap-permit2/interfaces/IPermit2.sol";
+import {IUniversalRouter} from "@uniswap-universal-router/interfaces/IUniversalRouter.sol";
 import {Currency} from "@uniswap-v4-core/types/Currency.sol";
 import {PoolKey} from "@uniswap-v4-core/types/PoolKey.sol";
+import {IPositionManager} from "@uniswap-v4-periphery/interfaces/IPositionManager.sol";
 import {IUniswapV4Migrator} from "../interfaces/IUniswapV4Migrator.sol";
-import {UniswapV4Proxy} from "../libraries/UniswapV4Proxy.sol";
+import {UniswapV4Library} from "../libraries/UniswapV4Library.sol";
 import {Migrator} from "./Migrator.sol";
 
 /// @title UniswapV4Migrator
 /// @notice Abstract contract for migrating positions between chains using Uniswap V4
 abstract contract UniswapV4Migrator is IUniswapV4Migrator, IERC721Receiver, Migrator {
-    UniswapV4Proxy private proxy;
+    IPositionManager private immutable positionManager;
+    IUniversalRouter private immutable universalRouter;
+    IPermit2 private immutable permit2;
+    mapping(Currency => bool) isPermit2Approved;
 
     /// @notice Constructor for the UniswapV4Migrator contract
-    /// @param positionManager The position manager address
-    /// @param universalRouter The universal router address
-    /// @param permit2 The permit2 address
-    constructor(address positionManager, address universalRouter, address permit2) {
-        proxy.initialize(positionManager, universalRouter, permit2);
+    /// @param _positionManager The position manager address
+    /// @param _universalRouter The universal router address
+    /// @param _permit2 The permit2 address
+    constructor(address _positionManager, address _universalRouter, address _permit2) {
+        positionManager = IPositionManager(_positionManager);
+        universalRouter = IUniversalRouter(_universalRouter);
+        permit2 = IPermit2(_permit2);
     }
 
     /// @notice Callback function for ERC721 tokens
@@ -28,7 +36,7 @@ abstract contract UniswapV4Migrator is IUniswapV4Migrator, IERC721Receiver, Migr
     /// @param data The data of the transfer
     /// @return selector The selector of the callback
     function onERC721Received(address, address from, uint256 tokenId, bytes memory data) external returns (bytes4) {
-        if (msg.sender != address(proxy.positionManager)) revert NotPositionManager();
+        if (msg.sender != address(positionManager)) revert NotPositionManager();
 
         _migrate(from, tokenId, data);
 
@@ -48,7 +56,8 @@ abstract contract UniswapV4Migrator is IUniswapV4Migrator, IERC721Receiver, Migr
         returns (address token0, address token1, uint256 amount0, uint256 amount1, bytes memory poolInfo)
     {
         PoolKey memory poolKey;
-        (poolKey, amount0, amount1) = proxy.liquidatePosition(positionId, 0, 0, address(this));
+        (poolKey, amount0, amount1) =
+            UniswapV4Library.liquidatePosition(positionManager, positionId, 0, 0, address(this));
 
         token0 = Currency.unwrap(poolKey.currency0);
         token1 = Currency.unwrap(poolKey.currency1);
@@ -67,7 +76,9 @@ abstract contract UniswapV4Migrator is IUniswapV4Migrator, IERC721Receiver, Migr
     {
         PoolKey memory poolKey = abi.decode(poolInfo, (PoolKey));
 
-        amountOut = proxy.swap(poolKey, zeroForOne, amountIn, 0, address(this));
+        amountOut = UniswapV4Library.swap(
+            universalRouter, permit2, isPermit2Approved, poolKey, zeroForOne, amountIn, 0, address(this)
+        );
     }
 
     /// @notice Receive function to allow the contract to receive native tokens, which are supported in Uniswap v4
