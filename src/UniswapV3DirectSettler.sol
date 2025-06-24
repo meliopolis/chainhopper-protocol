5f86bf32dddc3e1b9e319a250dc7840994ea2233
Aseem Sood <asood123@users.noreply.github.com>
2025-06-24 13:50:15 -0400

    cleaned up file names, compiler warnings

--- src/UniswapV3DirectSettler.sol
+++ src/UniswapV3DirectSettler.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import {DirectTransferSettler} from "./base/DirectTransferSettler.sol";
+import {DirectSettler} from "./base/DirectSettler.sol";
 import {Settler} from "./base/Settler.sol";
 import {UniswapV3Settler} from "./base/UniswapV3Settler.sol";
 
-/// @title UniswapV3DTSettler
-/// @notice A settler that settles migrations on Uniswap V3 and Across
-contract UniswapV3DTSettler is UniswapV3Settler, DirectTransferSettler {
+/// @title UniswapV3DirectSettler
+/// @notice A settler that settles migrations on Uniswap V3
+contract UniswapV3DirectSettler is UniswapV3Settler, DirectSettler {
     /// @notice Constructor
     /// @param initialOwner The initial owner of the settler
     /// @param positionManager The position manager
@@ -15,7 +15,7 @@ contract UniswapV3DTSettler is UniswapV3Settler, DirectTransferSettler {
     /// @param permit2 The permit2 contract
     constructor(address initialOwner, address positionManager, address universalRouter, address permit2)
         UniswapV3Settler(positionManager, universalRouter, permit2)
-        DirectTransferSettler()
+        DirectSettler()
         Settler(initialOwner)
     {}
 }
