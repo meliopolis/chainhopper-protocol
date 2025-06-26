a97505d10913c6e2ce6e136ac7beb54a37651f6c
Aseem Sood <asood123@users.noreply.github.com>
2025-06-26 11:47:35 -0400

    AerodromeLibraryTest

--- test/utils/TestContext.sol
+++ test/utils/TestContext.sol
@@ -31,6 +31,7 @@ contract TestContext is Test {
     IAcrossSpokePool internal acrossSpokePool;
     IPermit2 internal permit2;
     IUniversalRouter internal universalRouter;
+    IUniversalRouter internal aerodromeRouter;
     IV3PositionManager internal v3PositionManager;
     IV4PositionManager internal v4PositionManager;
     IStateView internal v4StateView;
@@ -55,6 +56,7 @@ contract TestContext is Test {
         permit2 = IPermit2(vm.envAddress(string(abi.encodePacked(srcChainName, "_UNISWAP_PERMIT2"))));
         universalRouter =
             IUniversalRouter(vm.envAddress(string(abi.encodePacked(srcChainName, "_UNISWAP_UNIVERSAL_ROUTER"))));
+        aerodromeRouter = IUniversalRouter(vm.envAddress(string(abi.encodePacked(srcChainName, "_AERODROME_UNIVERSAL_ROUTER"))));
         v3PositionManager =
             IV3PositionManager(vm.envAddress(string(abi.encodePacked(srcChainName, "_UNISWAP_V3_POSITION_MANAGER"))));
         v4PositionManager =
